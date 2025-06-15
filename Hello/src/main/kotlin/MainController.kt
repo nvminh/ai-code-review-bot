361e1159f7432422048830edeca638c54875dacd
Minh Nguyen <nvminh@gmail.com>
2025-06-15 11:58:22 +0700

    update hello project

--- Hello/src/main/kotlin/MainController.kt
+++ Hello/src/main/kotlin/MainController.kt
@@ -1,9 +1,10 @@
 package hello
 
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class MainController {
     @GetMapping("/hello")
     fun hello(@RequestParam name: String): String {
